32fb91cd39a2cd9089579f212b31a05bcaa4a01e
Wisn <wuyishun_kmk@outlook.com>
2018-05-06 19:45:30 +0800

    update module

---
+++ library/src/main/java/com/laiyifen/library/base/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.laiyifen.library.base;
+
+/**
+ * Created by Wisn on 2018/5/6 上午10:20.
+ */
+
+public class BaseFragment {
+}
---
+++ module_2/src/main/java/com/laiyifen/module_2/Module2Activity.java
@@ -0,0 +1,17 @@
+package com.laiyifen.module_2;
+
+import android.os.Bundle;
+
+import com.laiyifen.library.base.BaseActivity;
+
+/**
+ * Created by Wisn on 2018/5/5 下午8:56.
+ */
+
+public class Module2Activity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module2_activity);
+    }
+}
