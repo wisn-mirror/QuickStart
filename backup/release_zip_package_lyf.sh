19d0a8573b053f37f4314b9c245397bbbdc18e18
Wisn <wuyishun_kmk@outlook.com>
2019-04-19 18:51:23 +0800

    update

--- backup/release_zip_package_lyf.sh
+++ backup/release_zip_package_lyf.sh
@@ -1,31 +1,32 @@
 git config --global http.postBuffer 1048576000
 
+WORKSPACERoot=/Users/mac/Desktop/githubworkspace/QuickStart
 WORKSPACE=/Users/mac/Desktop/githubworkspace/QuickStart/app
 jgPath=/Users/mac/Desktop/tool/360jiagu/jiagu
-keystorePath=/Users/mac/Desktop/Android_lyf_ody/lyf_app/keystore
+keystorePath=/Users/mac/Desktop/backup/Android_lyf_ody/lyf_app/keystore
 wallePath=/Users/mac/Desktop/tool/walle/walle-cli
 buildtools25=/Users/mac/Library/Android/sdk/build-tools/25.0.2
-backup_Dir=/Users/mac/Desktop/Version/testbackup
+backup_Dir=/Users/mac/Desktop/Version/test/debug
 BuildLog=/Users/mac/Desktop/Version
 
-cd ${WORKSPACE}
-cd ..
+cd ${WORKSPACERoot}
 
 echo 开始打包
-sh ./gradlew clean assembleRelease | grep -v 'testReleasebuildlog.txt' > ${BuildLog}/testReleasebuildlog.txt
+sh ./gradlew clean assembleDebug | grep -v 'newDebugbuildlog.txt' > ${BuildLog}/newDebugbuildlog.txt
 while read channelsline
 do
 if echo "$channelsline"|grep -q -E "^:"
 then
 continue
-elif [ "$channelsline" = "BUILD SUCCESSFUL" ];then
+elif [[ "$channelsline" = "BUILD SUCCESSFUL" ]];then
 echo 'BUILD SUCCESSFUL'
 break
-elif [ "$channelsline" = "BUILD FAILED" ];then
+elif [[ "$channelsline" = "BUILD FAILED" ]];then
 echo 'BUILDFAILED'
+cat ${BuildLog}/newDebugbuildlog.txt
 exit
 fi
-done <${BuildLog}/testReleasebuildlog.txt
+done <${BuildLog}/newDebugbuildlog.txt
 
 
 
@@ -40,11 +41,13 @@ rm -rf ${WORKSPACE}/build/outputs/jiagu
 mkdir ${WORKSPACE}/build/outputs/jiagu
 #加固
 echo 开始加固
-ls ${WORKSPACE}/build/outputs/apk/release/ |grep -v 'apkjiagulist.txt' > ${WORKSPACE}/build/apkjiagulist.txt
+ls ${WORKSPACE}/build/outputs/apk/debug/ |grep -v 'apkjiagulist.txt' > ${WORKSPACE}/build/apkjiagulist.txt
 while read lineapk
 do
 echo ${WORKSPACE}/build/outputs/apk/${lineapk}
-java -jar ${jgPath}/jiagu.jar -jiagu ${WORKSPACE}/build/outputs/apk/release/${lineapk} ${WORKSPACE}/build/outputs/jiagu/ -autosign
+if [[ "${lineapk##*.}"x = "apk"x ]];then
+java -jar ${jgPath}/jiagu.jar -jiagu ${WORKSPACE}/build/outputs/apk/debug/${lineapk} ${WORKSPACE}/build/outputs/jiagu/ -autosign
+fi
 done < ${WORKSPACE}/build/apkjiagulist.txt
 echo 加固结束
 
@@ -66,7 +69,6 @@ exit
 echo zipalign4kok
 fi
 
-
 #签名
 echo startsign
 ls ${WORKSPACE}/build/outputs/duiqi/ |grep -v 'apksign.txt' > ${WORKSPACE}/build/apksign.txt
@@ -76,7 +78,6 @@ echo ${WORKSPACE}/build/outputs/duiqi/${linesign}
 ${buildtools25}/apksigner sign --ks   ${keystorePath}/laiyifen.key --ks-key-alias laiyifen --ks-pass pass:laiyifen  ${WORKSPACE}/build/outputs/duiqi/${linesign}
 done < ${WORKSPACE}/build/apksign.txt
 echo signsuccess
-
 
 apkName
 uploadPath
@@ -98,18 +99,30 @@ then
 continue
 fi
 substr=${channelsline%#*}
-echo 渠道${substr}start
+#echo 开始渠道${substr}start
 substr=$(echo $substr)
-echo ${WORKSPACE}/build/outputs/channels/${substr}_${apkName}
+echo 生成渠道${WORKSPACE}/build/outputs/channels/${substr}_${apkName}
 uploadPath=${WORKSPACE}/build/outputs/channels/${substr}_${apkName}
 java -jar ${wallePath}/walle-cli-all.jar put -c ${substr} ${WORKSPACE}/build/outputs/duiqi/${apkName}   ${uploadPath}
-echo 渠道${substr}end
-done <${WORKSPACE}/channel
+#echo 渠道${substr}end
+done <${WORKSPACERoot}/channel.txt
+
+
+#签名
+echo startsign
+ls ${WORKSPACE}/build/outputs/channels/ |grep -v 'apksign.txt' > ${WORKSPACE}/build/apksign.txt
+while read linesign
+do
+echo sign ${WORKSPACE}/build/outputs/channels/${linesign}
+${buildtools25}/apksigner sign --ks   ${keystorePath}/laiyifen.key --ks-key-alias laiyifen --ks-pass pass:laiyifen  ${WORKSPACE}/build/outputs/channels/${linesign}
+done < ${WORKSPACE}/build/apksign.txt
+echo signsuccess
+
 
 #上传
-curl -F "file=@"${uploadPath} -F "uKey=ed639f2e5cac76e08c1eb24b775c2b69" -F "_api_key=30ddd93225add3ed02677a8ae9722d80" https://qiniu-storage.pgyer.com/apiv1/app/upload
+curl -F "file=@"${uploadPath} -F "updateDescription=内测体验版" -F "uKey=ed639f2e5cac76e08c1eb24b775c2b69" -F "_api_key=30ddd93225add3ed02677a8ae9722d80" https://qiniu-storage.pgyer.com/apiv1/app/upload
 
 #备份
 rm -rf ${backup_Dir}/
 mkdir ${backup_Dir}/
-#cp -R ${WORKSPACE}/build/* ${backup_Dir}/
+cp -R ${WORKSPACE}/build/* ${backup_Dir}/
